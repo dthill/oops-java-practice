76b0289699cabb1182ea23b47f7506d075e4aacc
d <dthill@gmx.net>
2022-03-27 07:02:55 -0700

    wip

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Calculator {
+	private Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Calculator().calculate();
 
 	}
 
+	void calculate(){
+
+	}
+
+	double getOperant(){
+		double operant = 0.0;
+		while(true){
+			try {
+				operant = scanner.nextDouble();
+				break;
+			} catch (InputMismatchException exception) {
+				System.out.println("Please provide a valid operant");
+			} 
+		}
+		return operant;
+	}
+
+	
 }
